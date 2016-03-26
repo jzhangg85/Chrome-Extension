b23c0438bba827fd1d039e8daaa86ce7fef40e81
Jesse Zhang <jessezhang@college.harvard.edu>
2016-03-26 02:37:21 -0400

    finished

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Chrome-Extension
+################################
+README.md 
+################################ 
+
+This implementation of a Chrome Extension processes an active tab containing an article and outputs the article's title, url, summary, keywords, and character count. The extension utilizes the text processing API SMMRY.  
+
+
+################################ 
+INSTRUCTIONS AND FUNCTIONALITY
+################################ 
+From the GitHub page, clone the repository (a folder named article_summarizer) containing four files: manifest.json, popup.html, popup.js, and icon.png. Open up the Chrome browser, open More Tools -> Extensions. In the Extensions tab, click "Load unpacked extension." Select the folder named article_summarizer. You should see an icon of a red book appear next to the URL bar in the browser. 
+
+While on a long article, click the red icon and the "Summarize Article" button. The extension will return the article summary with other relevant information from the article.  
+
+Note that in the limited usage of the text processing API SMMRY, the API is limited to 10 requests daily. 
+
+
+################################ 
+CONSIDERATIONS
+################################ 
+If given more time, the extension's API key for SMMRY would be encrypted for security purposes. Also, the functionality of the extension would be increased to store a history of summarized articles using a web database API.   
--- popup.js
+++ popup.js
@@ -1,21 +1,55 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var saveActionButton = document.getElementById('saveActionId');
-  saveActionButton.addEventListener('click', function() {
-
+  
+  // set variable to gather element from button in html
+  var checkPageButton = document.getElementById('checkPage');
+  
+  // listen for button click and run callback function
+  checkPageButton.addEventListener('click', function() {
+    
+    // select active tab
     chrome.tabs.getSelected(null, function(tab) {
-      d = document; 
-      var url_link = tab.url 
+      
+      // make AJAX call
+      $.ajax({
+        
+        // create GET request to API key with specified parameters
+        method: "GET",
+        dataType: "json",
+        url: "http://api.smmry.com/SM_API_KEY=FE22353752&SM_LENGTH=4&SM_QUOTE_AVOID&SM_KEYWORD_COUNT=5&SM_URL=" + tab.url
+      })
+      
+      // upon data return from API key 
+      .done(function( data ) {
+        
+        // initalize empty body for output
+        var b = $("body")
+        b.empty();
+        
+        // store and append article title to body
+        var h1 = $("<h1>");
+        h1.text(data["sm_api_title"]);
+        b.append(h1);
+        
+        // store and append article url to body
+        var h2 = $("<h2>");
+        h2.text(tab.url);
+        b.append(h2);
 
-      var f = d.createElement('form');
-      f.action = 'smmry.com';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
+        // store and append article summary content to body
+        var p1 = $("<p>");
+        p1.text(data["sm_api_content"]);
+        b.append(p1); 
+
+        // store and append keywords to body
+        var p2 = $("<p>");
+        p2.text("Keywords: " + data["sm_api_keyword_array"].join(", "));
+        b.append(p2); 
+
+        // store and append character count to body
+        var p3 = $("<p>");
+        p3.text("Character count: " + data["sm_api_character_count"]);
+        b.append(p3);        
+      });
     });
   }, false);
 }, false);
